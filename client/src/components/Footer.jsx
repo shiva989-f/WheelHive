c7408febf11d8a501fa2a9d203cd11fe8d7c8ad2
shiva989-f <kumarshiva05722@gmail.com>
2025-02-17 19:23:03 +0530

    Added Footer

---
+++ client/src/components/Footer.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Footer = () => {
+    return (
+        <div className='w-full py-4 mt-10 shadow-md bg-primary flex flex-col justify-center items-center'>
+            <p className='text-lg text-white'>Terms of Use Privacy Policy</p>
+            <p className='text-base text-[#cacaca]'>Copyright &copy; 2025 WheelHive</p>
+        </div>
+    )
+}
+
+export default Footer
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -3,16 +3,20 @@ import CarSearchBar from '../components/CarSearchBar'
 import HeroComponent from '../components/HeroComponent'
 import LoadCar from '../components/LoadCar'
 import LogoutPopup from '../components/LogoutPopup'
+import Footer from '../components/Footer'
 
 const Home = () => {
     
     return (
-        <div className='px-2 font-poppins md:px-8 lg:px-12'>
-            {/* <Navbar /> */}
-            <HeroComponent onLogoutOpen={()=> setIsLogoutOpen(true)} />
-            <LoadCar/>
-            <LogoutPopup />
-            <ToastContainer />
+        <div>
+            <div className='px-2 font-poppins md:px-8 lg:px-12'>
+                {/* <Navbar /> */}
+                <HeroComponent onLogoutOpen={() => setIsLogoutOpen(true)} />
+                <LoadCar />
+                <LogoutPopup />
+                <ToastContainer />
+            </div>
+            <Footer/>
         </div>
     )
 }
